47cea82c62b1210b38638d8027f405614eaa2e2d
Marek <marek@iMac-de-Marek.local>
2024-05-21 10:08:26 +0200

    feat: home page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,61 +1,77 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div className="flex flex-col justify-center px-64">
+    <div>
       <h1 className="text-center text-3xl py-12 font-bold">
         🏖 &nbsp; Welcome to Travel Agency &nbsp; 🏖
       </h1>
-      <img
-        className="rounded"
-        src="https://images.pexels.com/photos/2166553/pexels-photo-2166553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="traveler"
-      />
-      <p className="my-4">
-        Your gateway to unforgettable adventures around the world! At Travel
-        Agency, every trip is a story to tell, an experience to live, and
-        memories to cherish forever.
-      </p>
-
-      <p className="my-4">
-        We are much more than just a travel agency; we are dream artisans,
-        creators of magical moments, and facilitators of bold explorations.
-        Whether you dream of getting lost in the bustling streets of a
-        cosmopolitan city, relaxing on white sandy beaches bathed in turquoise
-        waters, or discovering hidden treasures in distant lands, we are here to
-        bring your deepest aspirations to life.
-      </p>
-
-      <p className="my-4">
-        Our team of passionate experts is dedicated to offering you tailor-made
-        trips, perfectly adapted to your preferences and lifestyle. Whether
-        you're a solo traveler in search of adventure, a couple longing for
-        romance, a family in search of precious moments together, or a group of
-        friends seeking new experiences, we have the expertise to turn your
-        dreams into reality.
-      </p>
-
-      <p className="my-4">
-        As "Exotic Travel", we believe that every journey is an opportunity to
-        discover, learn, and grow. That's why we are committed to offering you
-        authentic, environmentally friendly, and socially responsible
-        experiences. Our commitment to sustainable development is reflected in
-        each of our itineraries, where we prioritize partnerships with local
-        suppliers, support local communities, and work to preserve biodiversity.
-      </p>
-
-      <p className="my-4">
-        Whether you're an experienced explorer in search of exciting challenges,
-        an intrepid traveler seeking new frontiers to conquer, or a romantic
-        dreamer in search of magical moments, "Exotic Travel" is your ideal
-        travel companion to create unforgettable memories that will last a
-        lifetime.
-      </p>
-
-      <p className="my-4">
-        Get ready to soar to new horizons, explore fascinating cultures, taste
-        exquisite flavors, and create memories that will accompany you forever.
-        Your adventure begins here, with "Exotic Travel". Embark with us and let
-        yourself be carried away to endless horizons!
-      </p>
+      <h2 className="text-center text-3xl py-3 font-semibold">
+        Que recherchez vous ?
+      </h2>
+      <div className="flex justify-around py-12">
+        <Link
+          to="/flights"
+          className="font-medium w-32 h-11 rounded flex border-solid justify-center place-items-center bg-green"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+            />
+          </svg>
+          Flights
+        </Link>
+        <Link
+          to="/hotels"
+          className="font-medium w-32 h-11 rounded flex border-solid justify-center place-items-center bg-green"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+          Hotels
+        </Link>
+        <Link
+          to="/activities"
+          className="font-medium w-32 h-11 rounded flex border-solid justify-center place-items-center bg-green"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+            />
+          </svg>
+          Activities
+        </Link>
+      </div>
+      <img src="https://images.pexels.com/photos/2765869/pexels-photo-2765869.jpeg?auto=compress&cs=tinysrgb&w=600" className="pb-12 mx-auto block rounded-xl"/>
     </div>
   );
 }
